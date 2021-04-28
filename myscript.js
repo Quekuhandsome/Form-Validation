e85c4eb3a6087249057768bff19da417481991cb
quekuhandsome <yaotsewonder@gmail.com>
2021-04-28 14:06:32 +0000

    form validation

---
+++ myscript.js
@@ -0,0 +1,5 @@
+//document.getElementById("heading").innerHTML =
+// "This Page is purposely for Revission";
+
+//document.getElementById("para1").innerHTML =
+// "lLorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis, modiCum, sequi natus enim maiores harum molestiae quod, quae quasi fugiatdebitis praesentium doloribus? Enim officia ea consectetur ab asperiores ";
---
+++ new.js
@@ -0,0 +1,6 @@
+let firstHtml = document.getElementById("frst");
+let secondHtml = document.getElementById("scnd");
+
+function myButt() {
+  document.write(frst.value + " and " + scnd.value);
+}
